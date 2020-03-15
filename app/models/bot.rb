0df7957b66af3fd08495839753f3c3e92b75cc25
KHong <albtrossfacefall@gmail.com>
2020-03-15 17:43:54 -0400

    prepare twitter gem initialization

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -1,2 +1,9 @@
 class Bot < ApplicationRecord
+  def self.interval_tweet
+    # alert = Alert.all.find {|a|a.time == Time.new().min}
+    # if alert
+    #   CLIENT.update(alert.minutes)
+    # end
+    CLIENT.update("#{Time.now.min} minutes")
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def main
+  make_cases
+end
+
+def make_cases
+  get_json("caseData").each do |a|
+    Case.create({
+      title: a["title"],
+      date: parse_date(a["date"]),
+      link: a["link"],
+      info: a["info"],
+      impact: a["impact"]
+    })
+  end
+end
+
+def parse_date(string)
+  arr = string.split("-").map {|c|c.to_i}
+  Date.new(arr[2], arr[0], arr[1])
+end
+
+def get_json(name)
+  path = File.dirname(__FILE__) + "/data/#{name}.json"
+  JSON.parse(File.read(path))
+end
+
+main
